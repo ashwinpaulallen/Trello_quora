a186e81f0bb9c7a4de2cdc915f9b631c9ff04da6
ashwinpaulallen <ashwin23paul@outlook.com>
2019-11-10 20:50:00 +0530

    Endpoint 3, Delete answer and view all answer

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -3,16 +3,27 @@ package com.upgrad.quora.api.controller;
 import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/")
 public class AnswerController {
+
+    @Autowired
+    private AnswerBusinessService answerBusinessService;
+
+    @Autowired
+    private UserAuthBusinessService userAuthBusinessService;
+
+    @Autowired
+    private QuestionBusinessService questionBusinessService;
 
     //This endpoint is used to create an answer to a particular question
     // this ia a POST method
@@ -58,5 +69,55 @@ public class AnswerController {
 
         return new ResponseEntity<AnswerEditResponse>(answerEditResponse,HttpStatus.OK);
     }
-    
+
+    //This endpoint is used to delete an answer.
+    // Only the owner of the answer or admin can delete an answer.
+    @RequestMapping(method = RequestMethod.DELETE, path = "/answer/delete/{answerId}",  produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerDeleteResponse> deleteAnswer(@PathVariable("answerId") final String answerId, @RequestHeader("authorization") final String authorization)
+            throws AuthorizationFailedException, AnswerNotFoundException {
+
+        UserAuthEntity userAuthEntity = userAuthBusinessService.getUser(authorization);
+        UserEntity userEntity= userAuthEntity.getUser();
+        AnswerEntity answerEntity = answerBusinessService.getAnswerFromId(answerId);
+        AnswerEntity checkedAnswer;
+
+        if(userEntity.getRole().equalsIgnoreCase("admin")) {
+            checkedAnswer = answerEntity;
+        }
+        else {
+            checkedAnswer = answerBusinessService.checkAnswerBelongToUser(userAuthEntity, answerEntity);
+        }
+        AnswerEntity deletedAnswer = answerBusinessService.deleteAnswer(checkedAnswer);
+
+        AnswerDeleteResponse answerDeleteResponse = new AnswerDeleteResponse().id(deletedAnswer.getUuid())
+                .status("ANSWER DELETED");
+
+        return new ResponseEntity<AnswerDeleteResponse>(answerDeleteResponse,HttpStatus.OK);
+    }
+
+    //This endpoint is used to get all answers to a particular question
+    @RequestMapping(method = RequestMethod.GET, path = "answer/all/{questionId}",  produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswersToQuestion(@PathVariable("questionId") final String questionId, @RequestHeader("authorization") final String authorization)
+            throws AuthorizationFailedException, InvalidQuestionException {
+
+        UserAuthEntity userAuthEntity = userAuthBusinessService.getUser(authorization);
+        QuestionEntity questionEntity = questionBusinessService.validateQuestion(questionId);
+
+
+        ArrayList<AnswerDetailsResponse> list = null;
+        ArrayList<AnswerEntity> rawList = (ArrayList) answerBusinessService.getAllAnswers(questionId , userAuthEntity);
+
+        for(AnswerEntity answer : rawList)
+        {
+            AnswerDetailsResponse detailsResponse = new AnswerDetailsResponse();
+            detailsResponse.setId(answer.getUuid());
+            detailsResponse.setAnswerContent(answer.getAnswer());
+            detailsResponse.setQuestionContent(questionEntity.getContent());
+            list.add(detailsResponse);
+        }
+
+        return new ResponseEntity<List<AnswerDetailsResponse>>(list, HttpStatus.OK);
+
+    }
+
 }
