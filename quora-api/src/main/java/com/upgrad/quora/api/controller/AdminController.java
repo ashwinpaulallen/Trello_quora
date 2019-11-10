0ce293710f861e2b9dffc4277ef6fb862a49b0ec
ashwinpaulallen <ashwin23paul@outlook.com>
2019-11-10 21:09:30 +0530

    Added common user and Admin controller

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class AdminController {
+
+    @Autowired
+    private UserBusinessService userBusinessService;
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/admin/user/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<UserDeleteResponse> deleteUser(@PathVariable("userId") final String uuid,
+                                                         @RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, UserNotFoundException {
+        userBusinessService.deleteUser(uuid,authorization);
+
+        UserDeleteResponse userDeleteResponse = new UserDeleteResponse().id(uuid).status("USER SUCCESSFULLY DELETED");
+        return new ResponseEntity<UserDeleteResponse>(userDeleteResponse, HttpStatus.OK);
+    }
+}
