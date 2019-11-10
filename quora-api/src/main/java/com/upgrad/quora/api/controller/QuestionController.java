c48f7e0f52399e46d4013be5f9a1317c19587ffe
ashwinpaulallen <ashwin23paul@outlook.com>
2019-11-10 19:08:23 +0530

    Endpoint 2, create get all qestions method

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -36,8 +36,21 @@ public class QuestionController {
         final ZonedDateTime now = ZonedDateTime.now();
         questionEntity.setDate(now);
 
-        final QuestionEntity createdQuestion = questionBusinessService.createQuestion(questionEntity , userAuthEntity);
+        final QuestionEntity createdQuestion = questionBusinessService.createQuestion(questionEntity, userAuthEntity);
         QuestionResponse questionResponse = new QuestionResponse().id(createdQuestion.getUuid()).status("QUESTION CREATED");
 
         return new ResponseEntity<QuestionResponse>(questionResponse, HttpStatus.CREATED);
     }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/question/all" , produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<QuestionDetailsResponse>> getAllQuestion(@RequestHeader("authorization") final String authorization) throws AuthorizationFailedException {
+
+        final UserAuthEntity userAuthEntity = userAuthBusinessService.getUser(authorization);
+
+        final List<QuestionEntity> allQuestion = questionBusinessService.getAllQuestion(userAuthEntity);
+
+        List<QuestionDetailsResponse> questionResponse = questionslist(allQuestion);
+
+        return new ResponseEntity<List<QuestionDetailsResponse>>(questionResponse, HttpStatus.OK);
+    }
+}
