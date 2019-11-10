a1531d1656364c090c105a0275df55a628cb1cdc
ashwinpaulallen <ashwin23paul@outlook.com>
2019-11-10 19:37:30 +0530

    Endpoint 2, added edit delete logic

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;git
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
 
