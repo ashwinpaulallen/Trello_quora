e8be57e188b3b9e83fe7d3482e7fd38f0a0783e8
ashwinpaulallen <ashwin23paul@outlook.com>
2019-11-10 19:02:08 +0530

    Endpoint 2, create question and exception handling

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -0,0 +1,43 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.*;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/")
+public class QuestionController {
+
+
+    @Autowired
+    private QuestionBusinessService questionBusinessService;
+
+    @Autowired
+    private UserAuthBusinessService userAuthBusinessService;
+
+    @RequestMapping(method = RequestMethod.POST, path = "/question/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<QuestionResponse> createQuestion(@RequestHeader("authorization") final String authorization, final QuestionRequest questionRequest) throws AuthorizationFailedException {
+
+        final UserAuthEntity userAuthEntity = userAuthBusinessService.getUser(authorization);
+
+        QuestionEntity questionEntity = new QuestionEntity();
+        questionEntity.setUuid(UUID.randomUUID().toString());
+        questionEntity.setContent(questionRequest.getContent());
+        questionEntity.setUser(userAuthEntity.getUser());
+        final ZonedDateTime now = ZonedDateTime.now();
+        questionEntity.setDate(now);
+
+        final QuestionEntity createdQuestion = questionBusinessService.createQuestion(questionEntity , userAuthEntity);
+        QuestionResponse questionResponse = new QuestionResponse().id(createdQuestion.getUuid()).status("QUESTION CREATED");
+
+        return new ResponseEntity<QuestionResponse>(questionResponse, HttpStatus.CREATED);
+    }
---
+++ quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.upgrad.quora.api.exception;
+
+import com.upgrad.quora.api.model.ErrorResponse;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+public class RestExceptionHandler {
+
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public ResponseEntity<ErrorResponse> authenticationFailedException(AuthenticationFailedException exe, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.UNAUTHORIZED
+        );
+    }
+}
